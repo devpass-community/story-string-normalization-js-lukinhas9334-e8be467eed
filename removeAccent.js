413126801ce84c618288006c578f9e5278237d14
Lucas Ribeiro Nicoleti <lucasribeironicoleti@gmail.com>
2023-05-18 13:19:45 -0300

    Update removeAccent.js

--- removeAccent.js
+++ removeAccent.js
@@ -1,5 +1,10 @@
 function removeAccent(str) {
     // Add your solution here!
+    return str.normalize('NFD').replace(/[\u0300-\u036f]/g, '');
 }
+
+var minhaStr = 'Olá, mundo!';
+var strSAcentos = removeAccent(minhaStr);
+console.log(strSAcentos);
 
 module.exports = removeAccent;
